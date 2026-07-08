672300bf0b40f72d04c15534abbf6ffc0a81d7a7
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-07-08 01:42:13 -0400

    feat: add route names

---
+++ app/lib/core/routing/route_names.dart
@@ -0,0 +1,8 @@
+class RouteNames {
+  const RouteNames._();
+
+  static const home = '/';
+  static const workspaces = '/workspaces';
+  static const missionControl = '/workspaces/:workspaceId/mission-control';
+  static const tasks = '/workspaces/:workspaceId/tasks';
+}
