4ae58e1a6a39fbcd4d062e560312e61f1b76513a
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-07-20 22:43:58 -0400

    Load state dropdown styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./polish.css";
+import "./state-select.css";
 
 export const metadata: Metadata = {
   title: "Melanated Adventurers App",
