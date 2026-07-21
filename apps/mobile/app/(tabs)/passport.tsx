152077653d9c88da25baf1d9b940ef3274f2c00e
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-07-21 03:36:25 -0400

    Add Passport route shell

---
+++ apps/mobile/app/(tabs)/passport.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderScreen } from '../../src/components/PlaceholderScreen';
+
+export default function PassportScreen() {
+  return <PlaceholderScreen title="Passport" description="Experience history, stamps, milestones, reflections, and Journey records will live here." />;
+}
