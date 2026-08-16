00f3aa038156c0608ed0459244c47db304a8b800
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-08-16 19:17:15 -0400

    add guided tutorial controller

---
+++ apps/mobile/src/onboarding/tutorialController.ts
@@ -0,0 +1,12 @@
+type TutorialListener = () => void;
+
+const listeners = new Set<TutorialListener>();
+
+export function startGuidedTutorial() {
+  listeners.forEach((listener) => listener());
+}
+
+export function subscribeGuidedTutorial(listener: TutorialListener) {
+  listeners.add(listener);
+  return () => listeners.delete(listener);
+}
