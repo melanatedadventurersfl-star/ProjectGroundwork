d9b2cf5fa844d39f99b19a3bacd04b64f0afcb7a
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-08-17 16:45:53 -0400

    Assemble Explorer shoreline banner asset

---
+++ apps/mobile/src/trailhead/assets/explorerShoreMorning.ts
@@ -0,0 +1,6 @@
+import m1 from './explorerShoreMorning.m1';
+import m2 from './explorerShoreMorning.m2';
+import m3 from './explorerShoreMorning.m3';
+import m4 from './explorerShoreMorning.m4';
+
+export default `data:image/jpeg;base64,${m1}${m2}${m3}${m4}`;
