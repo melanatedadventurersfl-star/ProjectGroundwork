fd6f82bd50efe531029c72bf98e5863ac4901b57
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-08-17 15:35:39 -0400

    Assemble full-resolution Explorer Trailhead asset

---
+++ apps/mobile/src/trailhead/assets/explorerHighRes.ts
@@ -0,0 +1,9 @@
+import h1 from './explorerHighRes.h1';
+import h2 from './explorerHighRes.h2';
+import h3 from './explorerHighRes.h3';
+import h4 from './explorerHighRes.h4';
+import h5 from './explorerHighRes.h5';
+import h6 from './explorerHighRes.h6';
+import h7 from './explorerHighRes.h7';
+
+export default `data:image/jpeg;base64,${h1}${h2}${h3}${h4}${h5}${h6}${h7}`;
