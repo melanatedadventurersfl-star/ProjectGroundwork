4423c7f044d88baf8aa0642c8414640b563ede66
melanatedadventurersfl-star <melanatedadventurersfl@gmail.com>
2026-08-17 17:51:35 -0400

    Add tenure badge data URI registry

---
+++ apps/mobile/src/passport/TenureBadgeDataUris.ts
@@ -0,0 +1,5 @@
+export const tenureBadgeDataUris = {
+  'Year 3': 'data:image/png;base64,PLACEHOLDER_YEAR3',
+  'Year 4': 'data:image/png;base64,PLACEHOLDER_YEAR4',
+  'Year 5': 'data:image/png;base64,PLACEHOLDER_YEAR5',
+} as const;
